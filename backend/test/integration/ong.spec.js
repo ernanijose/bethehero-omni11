9669a82e219d3f56a727ccb2402310e594ae3d23
ernanijose <ernanijose@gmail.com>
2020-04-28 18:31:24 -0300

    Incluindo algumas dicas

--- backend/test/integration/ong.spec.js
+++ backend/test/integration/ong.spec.js
@@ -4,15 +4,18 @@ const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
     beforeEach(async () => {
+        await connection.migrate.rollback(); //antes de executar o migrate, é sempre bom executar o rollback
         await connection.migrate.latest();
     });
 
+    //caso precise der erro no banco de dados, pois ainda continua executando, então função abaixo fecha conexão com BD
     afterAll(async () => {
         await connection.destroy();
     });
     it('Should be able to create a new ONG', async () => {
         const response = await request(app)
         .post('/ongs')
+        //.set('Authorization', '670948a6') //caso precise fazer um teste para quando um headers
         .send({
             name: "APAD 4",
             email: "contato@apad.com.br",
